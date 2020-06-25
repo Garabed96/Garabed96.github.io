0c8f963043bcc40c040ab1bdaf5f6cbe3c1c0e31
Garo Nazarian <garonazarian@pop-os.localdomain>
2020-06-24 22:49:45 -0400

    preparing the frontend app

--- backend/apps.py
+++ backend/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WebsiteConfig(AppConfig):
-    name = 'website'
+    name = 'backend'
     verbose_name = "WebsiteConf"
--- backend/models.py
+++ backend/models.py
@@ -9,8 +9,15 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
 
+from django.db import models
 
-# # Posting images to the website
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# # Posting images to the backend
 #
 # image_storage = FileSystemStorage(
 #     # Physical file location ROOT
