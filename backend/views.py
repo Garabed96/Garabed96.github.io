bd3c29a4a1266bb72b02992039cda7a70d3b5a75
Garo Nazarian <garonazarian@pop-os.localdomain>
2020-06-25 20:12:44 -0400

    rest_framework implemented

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from .models import Lead
+from .serializers import LeadSerializer
+from rest_framework import generics
 
-# Create your views here.
-print("this is the base_dir: "+settings.BASE_DIR)
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
 
 def index(request):
     return render(request, 'index.html')
