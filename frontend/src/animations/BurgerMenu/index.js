1837144410a7310f80f3451f46781240ea5c1a84
Garabed96 <garo567@hotmail.com>
2020-08-07 00:30:08 -0400

    lifting state up in React with hooks

--- frontend/src/animations/BurgerMenu/index.js
+++ frontend/src/animations/BurgerMenu/index.js
@@ -10,38 +10,55 @@ function getMenuDropdown(props){
     return hamburgerStyle;
 };
 
-export const DropdownMenu = () => {
+export const DropdownMenu = (props) => {
     return(
         <div style={{"height": "0%"}}>
-            tester
+            {props.status}
         </div>
-        // <div className="overlay" style={getMenuDropdown()} >
+
+    );
+}
+
+// Lifting state up in React with hooks (this is how we share state)
+const MenuToggle = (props) => {
+    return(
+            <p> The toggle is set to {props.toggle} </p>
+        )
+}
+
+const BurgerMenu = () => {
+    // https://reactjs.org/docs/lifting-state-up.html
+    // This is a react hook, it allows you to use react states without writing a class!
+    const [status, setStatus] = useState('close')
+
+    return(
+        <div>
+            <div>
+                Example div ontop of the Burger
+            </div>
+            <nav>
+                <div
+                    className="BurgerMenu__container"
+                    role="button"
+                    onClick={() => setStatus(status === 'open' ? 'close' : 'open')}>
+                    <i className={status}></i>
+                    <i className={status}></i>
+                    <i className={status}></i>
+                    < MenuToggle
+                      toggle={status}  />
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+
+export default BurgerMenu;
+
+ // <div className="overlay" style={getMenuDropdown()} >
         //         <div className="overlay-content">
         //             <a href="#">about</a>
         //             <a href="#">portfolio</a>
         //             <a href="#">Contact</a>
         //         </div>
         // </div>
-    );
-}
-
-
-const BurgerMenu = () => {
-    const [status, setStatus] = useState('close')
-    return(
-        <nav>
-            <div
-                className="BurgerMenu__container"
-                role="button"
-                onClick={() => setStatus(status === 'open' ? 'close' : 'open')}>
-                <i className={status}></i>
-                <i className={status}></i>
-                <i className={status}></i>
-            </div>
-        </nav>
-
-    );
-};
-
-
-export default BurgerMenu;
