af13f299782d4cda2a17896e37981b38ae0fa61d
Garo Nazarian <garonazarian09@gmail.com>
2020-10-12 15:04:37 -0400

    example settings.py react added

---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\garo5\onedrive\desktop\myweb\mywebsite\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
